fdaa427a85ce7287041f8b4535bef9fb047db1d4
TxT <odouroflove@gmail.com>
2017-01-01 11:55:34 +0700

    Thêm file LoginActivity.java

---
+++ app/src/main/java/txt/com/video3_github/LoginActivity.java
@@ -0,0 +1,10 @@
+package txt.com.video3_github;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TxT on 01/01/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+}
